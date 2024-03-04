01fe94035476a9d9f2cf05b7fa5cc16deb0080f7
victorDev6 <victor7rodriguez6@gmail.com>
2024-03-04 12:58:25 -0600

    First commit

---
+++ force-app/main/default/lwc/accountSearch/accountSearch.js
@@ -0,0 +1,18 @@
+import { LightningElement, wire } from 'lwc';
+import queryAccountsByEmployeeNumber from '@salesforce/apex/AccountListControllerLwc.queryAccountsByEmployeeNumber';
+
+export default class AccountSearch extends LightningElement {
+  numberOfEmployees = null;
+
+  handleChange = (event) => {
+    this.numberOfEmployees = event.detail.value;
+  }
+
+  reset = () => {
+    console.log('reset!');
+    this.numberOfEmployees = null;
+  }
+
+  @wire(queryAccountsByEmployeeNumber, {numberOfEmployees: '$numberOfEmployees'})
+  accounts;
+}
---
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -0,0 +1,23 @@
+import { LightningElement, wire } from 'lwc';
+import { reduceErrors } from 'c/ldsUtils';
+import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
+import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
+import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+import getContacts from '@salesforce/apex/ContactController.getContacts';
+
+const COLUMNS = [
+  { label: 'FirstName', fieldName: FIRSTNAME_FIELD.fieldApiName, type: 'text' },
+  { label: 'LastName', fieldName: LASTNAME_FIELD.fieldApiName, type: 'text' },
+  { label: 'Email', fieldName: EMAIL_FIELD.fieldApiName, type: 'text' },
+];
+
+export default class ContactList extends LightningElement {
+  columns = COLUMNS;
+
+  @wire(getContacts)
+  contacts;
+
+  get errors() {
+    return (this.contacts.errors) ? reduceErrors(this.contacts.errors) : [];
+  }
+}
